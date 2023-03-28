821b41ac8a486bf3bb7e2d952513f84e011207cc
Chanapol <Chanapolpuntuvade1@gmail.com>
2023-03-28 13:46:00 +0700

    Add new

---
+++ src/exception1/Quotient.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exception1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author nitisakkangkhan
+ */
+public class Quotient {
+        public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter two integers : ");
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
+        try {
+            System.out.println(number1+ "/" +number2+ " is " +(number1 / number2));
+        }  
+        catch (ArithmeticException e){
+            System.out.println("Exception : Divisor cannot be zero");
+        }
+    }
+}
